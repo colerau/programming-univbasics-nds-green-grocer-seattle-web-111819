0d285584cbf8f2d713e7b0471817109ca146b822
Cole Rau <colevrau@gmail.com>
2019-11-18 10:11:54 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -80,7 +80,8 @@ def checkout(cart, coupons)
     total += final_cart[counter][:price] * final_cart[counter][:count]
     counter += 1 
   end 
-  if total 
+  if total > 100 
+    
   total 
 end 
   
